fba0f224a4614f0fa941041341c168403554acac
dainen-fujiwara <h.fujiwara@dainen.co.jp>
2023-10-27 21:25:01 +0900

    231027

---
+++ assets/js/script.js
@@ -0,0 +1,331 @@
+/*===========================================================*/
+/*機能編 5-1-16クリックしたら円形背景が拡大（上から）*/
+/*===========================================================*/
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
+/*===========================================================*/
+/*機能編 8-1-8リンクボタンをクリックしたら形状が変化*/
+/*===========================================================*/
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から100pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+	var scroll = $(window).scrollTop(); //スクロール値を取得
+	if(scroll > 0){
+		$(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
+            $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
+        });
+	}
+    return false;//リンク自体の無効化
+});
+
+
+/*===========================================================*/
+/*機能編 6-1-1横移動させて全画面で見せる*/
+/*===========================================================*/
+	$('.slider').slick({
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+		speed:500,//スライドの動きのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 1,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+		arrows:false,//左右の矢印なし
+		//prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+		//nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+		//dots: true,//下部ドットナビゲーションの表示
+        pauseOnFocus: false,//フォーカスで一時停止を無効
+        pauseOnHover: false,//マウスホバーで一時停止を無効
+        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+    $('.slider').slick('slickPlay');
+});
+
+/*===========================================================*/
+/*機能編 6-1-8複数画像を流して見せる*/
+/*===========================================================*/
+
+	$('.slider2').slick({
+		arrows: false,//左右の矢印はなし
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+		speed: 6900,//スライドのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+		pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+		cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+		slidesToShow: 4,//スライドを画面に4枚見せる
+		slidesToScroll: 1,//1回のスライドで動かす要素数
+		responsive: [
+			{
+			breakpoint: 769,//モニターの横幅が769px以下の見せ方
+			settings: {
+				slidesToShow: 2,//スライドを画面に2枚見せる
+			}
+		},
+		{
+			breakpoint: 426,//モニターの横幅が426px以下の見せ方
+			settings: {
+				slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+			}
+		}
+	]
+	});
+
+/*===========================================================*/
+/*印象編 4 最低限おぼえておきたい動き*/
+/*===========================================================*/
+
+function fadeAnime(){
+
+    //4-1 ふわっ（その場で）
+    $('.fadeInTrigger').each(function(){ //fadeInTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeIn');// 画面内に入ったらfadeInというクラス名を追記
+		}else{
+		$(this).removeClass('fadeIn');// 画面外に出たらfadeInというクラス名を外す
+		}
+		});
+    //4-1 ふわっ（下から）
+	$('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+		}else{
+		$(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+		}
+		});
+
+	//4-2 パタッ（左上へ）
+	$('.flipLeftTopTrigger').each(function(){ //flipLeftTopTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('flipLeftTop');// 画面内に入ったらflipLeftTopというクラス名を追記
+		}else{
+		$(this).removeClass('flipLeftTop');// 画面外に出たらflipLeftTopというクラス名を外す
+		}
+		});
+
+    //4-2 パタッ（右上へ）
+    $('.flipRightTopTrigger').each(function(){ //flipRightTopTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('flipRightTop');// 画面内に入ったらflipRightTopというクラス名を追記
+		}else{
+		$(this).removeClass('flipRightTop');// 画面外に出たらflipRightTopというクラス名を外す
+		}
+		});
+
+	// 4-4 ボンッ（拡大）
+	$('.zoomInTrigger').each(function(){ //zoomInTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('zoomIn');// 画面内に入ったらzoomInというクラス名を追記
+		}else{
+		$(this).removeClass('zoomIn');// 画面外に出たらzoomInというクラス名を外す
+		}
+		});
+}
+
+/*===========================================================*/
+/*印象編　9-2　PNG アニメーション（APNG）*/
+/*===========================================================*/
+
+APNG.ifNeeded().then(function () {
+	var images = document.querySelectorAll(".apng-image");
+	for (var i = 0; i < images.length; i++) APNG.animateImage(images[i]);
+});
+
+/*===========================================================*/
+/*関数をまとめる*/
+/*===========================================================*/
+
+var rotate = [];
+var set_position = 0;
+var pm = 1;
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime();//機能編 8-1-8	リンクボタンをクリックしたら形状が変化の関数を呼ぶ
+	fadeAnime();//印象編 4 最低限おぼえておきたい動きの関数を呼ぶ
+
+	if (set_position < document.documentElement.scrollTop) {
+		pm = -1;
+	} else {
+		pm = 1;
+	}
+
+	set_position = document.documentElement.scrollTop;
+	rotateParts(pm);	//文字を回転させる
+});
+
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load',function(){
+
+	$('.parts').each(function(i, e){
+		rotate[i+1] = getRotationDegrees($(e));
+	});
+
+	//テキストのカウントアップ+バーの設定
+	var bar = new ProgressBar.Line(splash_text, {//id名を指定
+		easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+		duration: 1000,//時間指定(1000＝1秒)
+		strokeWidth: 0.2,//進捗ゲージの太さ
+		color: '#fff',//進捗ゲージのカラー
+		trailWidth: 0.2,//ゲージベースの線の太さ
+		trailColor: '#aaa',//ゲージベースの線のカラー
+		text: {//テキストの形状を直接指定
+			style: {//天地中央に配置
+				position: 'absolute',
+				left: '50%',
+				top: '50%',
+				padding: '0',
+				margin: '-30px 0 0 0',//バーより上に配置
+				transform:'translate(-50%,-50%)',
+				'font-size':'1rem',
+				color: '#fff',
+			},
+			autoStyleContainer: false //自動付与のスタイルを切る
+		},
+		step: function(state, bar) {
+			bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+		}
+	});
+
+	//プログレスバーのアニメーションスタート
+	bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+		$("#splash_text").fadeOut(10);//フェードアウトでローディングテキストを削除
+		$(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+		$(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+
+	    //=====ここからローディングエリア（splashエリア）をフェードアウトした後に動かしたいJSをまとめる
+		$("#splash").fadeOut('slow',function(){//#splashエリアをフェードアウトした後にアニメーションを実行
+
+	    /* 印象編 8-11テキストが1文字づつ出現*/
+			//spanタグを追加する
+			var element = $(".eachTextAnime");
+			element.each(function () {
+				var text = $(this).text();
+				var textbox = "";
+				text.split('').forEach(function (t, i) {
+					if (t !== " ") {
+						if (i < 10) {
+							textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+						} else {
+							var n = i / 10;
+							textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+						}
+
+					} else {
+						textbox += t;
+					}
+				});
+				$(this).html(textbox);
+			});
+
+			EachTextAnimeControl();//印象編 8-11テキストが1文字づつ出現の関数を呼ぶ
+	    fadeAnime();//印象編 4 最低限おぼえておきたい動きの関数を呼ぶ
+
+	  }); //=====ここまでローディングエリア（splashエリア）を0.8秒でフェードアウトした後に動かしたいJSをまとめる
+	});//=====ここまでプログレスバー表示
+
+
+    /*===========================================================*/
+    /*機能編 4-2-9背景色が四角に拡大（四隅へ）*/
+    /*===========================================================*/
+
+    //=====ここから背景が伸びた後に動かしたいJSをまとめる
+    $('.splashbg').on('animationend', function() {
+        fadeAnime();//印象編 4 最低限おぼえておきたい動きの関数を呼ぶ
+    });
+    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+
+});// ここまでページが読み込まれたらすぐに動かしたい場合の記述
+
+
+// eachTextAnimeにappeartextというクラス名を付ける定義
+function EachTextAnimeControl() {
+	$('.eachTextAnime').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("appeartext");
+
+		} else {
+			$(this).removeClass("appeartext");
+		}
+	});
+}
+
+
+//文字の回転
+function rotateParts(pm){
+	$('.parts').each(function(i, e){
+		var key = i + 1;
+		if ( i % 2 == 1 ) {
+			rotate[key] += 5 * pm;
+		} else {
+			rotate[key] -= 5 * pm;
+		}
+		$(e).css({ transform: 'rotate(' + rotate[key] + 'deg)' })
+	});
+}
+
+//現在の回転位置取得
+function getRotationDegrees(obj) {
+    var matrix = obj.css("-webkit-transform") ||
+    obj.css("-moz-transform")    ||
+    obj.css("-ms-transform")     ||
+    obj.css("-o-transform")      ||
+    obj.css("transform");
+    if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+    return (angle < 0) ? angle + 360 : angle;
+}
